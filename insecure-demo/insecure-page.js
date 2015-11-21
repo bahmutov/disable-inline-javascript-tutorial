9535c7afd5f99b26033f251bce65cd4856e432f3
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-11-21 11:48:40 -0500

    example inserting script from the text area into DOM

--- insecure-demo/insecure-page.js
+++ insecure-demo/insecure-page.js
@@ -1,16 +1,23 @@
 (function insecurePage() {
   console.log('insecure-page script started');
 
-  function linkGoodInput() {
-    console.log('linking good content to the DOM');
-    var input = document.getElementById('goodInput');
+  function linkInput(sourceId) {
+    console.log('linking content from %s to the DOM', sourceId);
+    var input = document.getElementById(sourceId);
     var markup = input.value;
     console.log('linking markup\n' + markup);
-    input.insertAdjacentHTML('afterend', '<p>\n' + markup + '\n</p>\n');
+
+    // use jQuery for simplicity, but this is NOT 100% secure against
+    // script execution, see http://api.jquery.com/jquery.parsehtml/
+    var runScripts = true;
+    var html = $.parseHTML('<p>\n' + markup + '\n</p>\n', document, runScripts);
+    $(input).after(html);
   }
 
-  document.getElementById('linkGoodInput').addEventListener('click',
-    linkGoodInput);
+  document.getElementById('linkGoodInput')
+    .addEventListener('click', linkInput.bind(null, 'goodInput'));
+  document.getElementById('linkBadInput')
+    .addEventListener('click', linkInput.bind(null, 'badInput'));
 
   console.log('insecure-page script finished');
 }());
